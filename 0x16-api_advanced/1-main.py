f9af392923e1fd3e299842c2c5b6f6b0fd95dc91
Azeez Saka <saoconcept1@gmail.com>
2024-02-14 08:26:23 -0800

    added 1-top_ten.py script

---
+++ 0x16-api_advanced/1-main.py
@@ -0,0 +1,13 @@
+wintermancer@lapbox ~/reddit_api/project $ cat 1-main.py
+#!/usr/bin/python3
+"""
+1-main
+"""
+import sys
+
+if __name__ == '__main__':
+    top_ten = __import__('1-top_ten').top_ten
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
